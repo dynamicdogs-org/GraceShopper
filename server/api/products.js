b994b2d4ada0fdc85e50c8457a1e54ab42a2a985
Kevin Lam <find.keviin@gmail.com>
2019-06-11 17:28:23 -0400

    Created Products/ProductsId Route and Tests

---
+++ server/api/products.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const {User, Product} = require('../db/models/')
+
+//GET Route
+router.get('/', async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll()
+    res.json(allProducts)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const singleProduct = await Product.findById(req.params.productId)
+    res.json(singleProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
