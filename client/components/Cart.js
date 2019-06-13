8d33bc23896e51c01c67a202086c4dfea3f75979
dcnycoder <dc5stars@gmail.com>
2019-06-13 15:59:23 -0400

    created a cart reducer

---
+++ client/components/Cart.js
@@ -0,0 +1,10 @@
+import react from 'react'
+import {Component} from 'react'
+
+class disonnectedCart extends Component() {
+  render() {
+    return <div>This is User's cart</div>
+  }
+}
+
+//export
