e160a6596562d1f8ac209f8454e82a8fd8b6f2c5
Kevin Lam <find.keviin@gmail.com>
2019-06-15 17:37:08 -0400

    feat: Created submit order, thunk and reducer. Started checkoutform component

---
+++ client/components/CheckoutForm.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class CheckoutForm extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>CheckoutForm</h1>
+      </div>
+    )
+  }
+}
+
+export default CheckoutForm
---
+++ client/store/order.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+//action type
+const SUBMIT_ORDER = 'SUBMIT_ORDER'
+
+//action creators
+const submitOrder = orderData => {
+  return {
+    type: SUBMIT_ORDER,
+    payload: orderData
+  }
+}
+
+//thunks
+export const submitOrderThunk = function(order) {
+  return async function(dispatch) {
+    try {
+      const {data} = await axios.post('/api/orders', order)
+      dispatch(submitOrder(data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+//initial state
+const initialState = []
+
+//reducers
+const orderReducer = (orders = initialState, action) => {
+  switch (action.type) {
+    case SUBMIT_ORDER:
+      return [...orders, action.payload]
+    default:
+      return orders
+  }
+}
