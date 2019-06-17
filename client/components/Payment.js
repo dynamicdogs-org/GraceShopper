f8cb6126bd75d886053e2cc1996255a408355145
Terence Helsel <terence.helsel@gmail.com>
2019-06-17 16:32:40 -0400

    git feat: validateField function updates Payment state

--- client/components/Payment.js
+++ client/components/Payment.js
@@ -11,6 +11,10 @@ class Payment extends Component {
       cardNumber: '',
       expDate: '',
       cvv: '',
+      nameOnCardValid: false,
+      cardNumberValid: false,
+      expDateValid: false,
+      cvvValid: false,
       submitted: false
     }
     this.handleChange = this.handleChange.bind(this)
@@ -44,6 +48,28 @@ class Payment extends Component {
     this.setState(prevState => {
       return {submitted: !prevState.submitted}
     })
+  }
+
+  validateField(fieldname, value) {
+    let {nameOnCardValid, cardNumberValid, expDateValid, cvvValid} = this.state
+    switch (fieldName) {
+      case 'nameOnCard':
+        nameOnCardValid = value.match(/^[a-zA-z\s]+$/)
+        break
+      case 'cardNumber':
+        cardNumberValid = value.match(
+          /^(?:4[0-9]{12}(?:[0-9]{3})? | (?:5[1-5][0-9]{2} | 222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12} | 3[47][0-9]{13} | 3(?:0[0-5]|[68][0-9])[0-9]{11} | 6(?:011|5[0-9]{2})[0-9]{12} | (?:2131|1800|35\d{3})\d{11})$/
+        )
+        break
+      case 'expDateValid':
+        expDateValid = value > new Date()
+        break
+      case 'cvv':
+        cvvValid = value.match(/^[0-9]{3}/)
+        break
+      default:
+        break
+    }
   }
 
   render() {
