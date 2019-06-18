62741ca0b884be03130ee0c308d5010abb4aa970
Terence Helsel <terence.helsel@gmail.com>
2019-06-18 13:04:59 -0400

    test: carts routes accessible only to appropriate user

--- server/api/carts.spec.js
+++ server/api/carts.spec.js
@@ -7,11 +7,14 @@ const app = require('../index')
 const Cart = db.model('cart')
 const User = db.model('user')
 const Product = db.model('product')
+const session = require('supertest-session')
 
 describe('Cart routes', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
+
+  let authSession = session(app)
 
   describe('/api/carts/', () => {
     beforeEach(async () => {
@@ -28,6 +31,11 @@ describe('Cart routes', () => {
         firstName: 'Husky2',
         lastName: 'TheDog'
       })
+
+      await authSession
+        .post('/auth/login')
+        .send({email: 'husky1@bark.com', password: 'imahusky'})
+        .expect(200)
 
       const prod1 = await Product.create({
         name: 'Huskys Favorite Food',
@@ -75,41 +83,28 @@ describe('Cart routes', () => {
       })
     })
 
-    xit('GET /api/carts/:userId', async () => {
-      const res = await request(app)
-        .send({user: {id: 1}})
-        .get('/api/carts/1')
-        .expect(200)
+    it('GET /api/carts/:userId', async () => {
+      const res = await authSession.get('/api/carts/1').expect(200)
 
       expect(res.body).to.be.an('array')
       expect(res.body[0].id).to.be.equal(2)
+      expect(res.body[1].id).to.be.equal(3)
     })
 
-    xit('GET /api/carts/:userId', done => {
-      try {
-        request(app)
-          .post('/auth/login')
-          .send({email: 'husky1@bark.com', password: 'imahusky'})
-          .then(res => {
-            request(app)
-              .get('/api/carts/1')
-              .then(done())
-          })
-      } catch (error) {
-        done(error)
-      }
+    it("does not GET another user's cart", async () => {
+      const res = await authSession.get('/api/carts/2').expect(302)
+
+      expect(res.text).to.be.equal('Found. Redirecting to /notauthorized')
     })
 
     xit('POST /api/carts/1/3', async () => {
-      const res = await request(app)
-        .post('/api/carts/1/3')
-        .expect(201)
+      const res = await authSession.post('/api/carts/1/3').expect(201)
 
       expect(res.body.productId).to.be.equal(3)
     })
 
-    xit('PUT /api/carts/:userId/:productId', async () => {
-      const res = await request(app)
+    it('PUT /api/carts/:userId/:productId', async () => {
+      const res = await authSession
         .put('/api/carts/1/2')
         .send({quantity: 10})
         .expect(200)
@@ -117,24 +112,20 @@ describe('Cart routes', () => {
       expect(res.body.quantity).to.be.equal(10)
     })
 
-    xit('DELETE /api/carts/:userId/:productId', async () => {
-      const res = await request(app)
-        .delete('/api/carts/2/3')
-        .expect(204)
+    it('DELETE /api/carts/:userId/:productId', async () => {
+      const res = await authSession.delete('/api/carts/1/3').expect(204)
 
-      const users = await request(app).get('/api/carts/2')
+      const users = await authSession.get('/api/carts/1')
 
-      expect(users.body.length).to.be.equal(0)
+      expect(users.body.length).to.be.equal(1)
     })
 
-    xit('DELETE /api/carts/:userId', async () => {
-      const res = await request(app)
-        .delete('/api/carts/1')
-        .expect(204)
+    it('DELETE /api/carts/:userId', async () => {
+      const res = await authSession.delete('/api/carts/1').expect(204)
 
-      const user = await request(app).get('/api/carts/1')
+      const cart = await authSession.get('/api/carts/1')
 
-      expect(user.body.length).to.be.equal(0)
+      expect(cart.body.length).to.be.equal(0)
     })
   }) // end describe('/api/carts')
 }) // end describe('Carts routes')
